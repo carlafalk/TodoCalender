ec017da0426b11b754211f70fcf199818806f62c
Tommy Zarén <85tomzar@skola.boras.se>
2022-06-21 17:47:27 +0200

    changed layout. added city and icon

--- JS/variables.js
+++ JS/variables.js
@@ -31,6 +31,27 @@ function setNextMonth() {
   currentDate.setMonth(nextMonth);
 }
 
+const weatherIcon = {
+  "01d": "fa-sun",
+  "02d": "fa-cloud-sun",
+  "03d": "fa-cloud",
+  "04d": "fa-cloud",
+  "09d": "fa-cloud-showers-heavy",
+  "10d": "fa-cloud-sun-rain",
+  "11d": "fa-cloud-bolt",
+  "13d": "fa-snowflake",
+  "50d": "fa-smog",
+  "01n": "fa-moon",
+  "02n": "fa-cloud-moon",
+  "03n": "fa-cloud",
+  "04n": "fa-cloud",
+  "09n": "fa-cloud-showers-heavy",
+  "10n": "fa-cloud-moon-rain",
+  "11n": "fa-cloud-bolt",
+  "13n": "fa-snowflake",
+  "50n": "fa-smog",
+};
+
 // does all of these need to be globals? ⬇
 const calendarContainer = document.querySelector(".calendar-container");
 const todoListDiv = document.querySelector(".todo-list");
